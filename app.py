5f2c6593f48f3190ed11fc25116c97f24f7700e9
badrievad <zzdl4@bk.ru>
2024-08-21 16:04:02 +0300

    Добавил Query String для получения инфы по curr user

--- app.py
+++ app.py
@@ -13,42 +13,42 @@ app = FastAPI()
 class PDFRequest(BaseModel):
     calc_id: int
     user_login: str
+    user_name: str
+    user_email: str
+    user_phone: str
 
 
-async def generate_pdf_async(calc_id, user_login):
+async def generate_pdf_async(calc_id, data: PDFRequest):
     browser = await launch(
         executablePath="C:/Program Files/Google/Chrome/Application/chrome.exe"
     )
     page = await browser.newPage()
 
-    # Дождаться полной загрузки страницы, включая шрифты и изображения
-    await page.goto(f"{URL_TO_OFFER}/{calc_id}", {"waitUntil": "networkidle2"})
+    # Формирование URL с параметрами
+    full_url = f"{URL_TO_OFFER}/{calc_id}?user_login={data.user_login}&name={data.user_name}&email={data.user_email}&phone={data.user_phone}"
+    # Переход на страницу с параметрами
+    await page.goto(full_url, {"waitUntil": "networkidle2"})
 
     # Принудительно выполнить рендеринг всех шрифтов
     await page.evaluate(
         """() => {
-        document.fonts.ready.then(() => console.log('Fonts loaded'));
-    }"""
+            document.fonts.ready.then(() => console.log('Fonts loaded'));
+        }"""
     )
 
     file_name = f"Коммерческое предложение (id_{calc_id}).pdf"
-    output_pdf_path = os.path.join(PATH_TO_PDF, user_login, file_name)
+    output_pdf_path = os.path.join(PATH_TO_PDF, data.user_login, file_name)
 
-    # Создание директорий, если они не существуют
     os.makedirs(os.path.dirname(output_pdf_path), exist_ok=True)
 
-    # Создание PDF с включенным фоном
-    await page.pdf(
-        {
-            "path": output_pdf_path,
-            "format": "A4",
-            "printBackground": True,  # Включить фоновый цвет и изображения
-        }
-    )
+    await page.pdf({
+        "path": output_pdf_path,
+        "format": "A4",
+        "printBackground": True,
+    })
 
     await browser.close()
 
-    # Загрузка файла на Яндекс.Диск или S3
     yandex_upload_file_s3(output_pdf_path, file_name)
 
     return output_pdf_path
@@ -57,13 +57,12 @@ async def generate_pdf_async(calc_id, user_login):
 @app.post("/generate_pdf")
 async def generate_pdf(request: PDFRequest):
     calc_id = request.calc_id
-    user_login = request.user_login
 
-    if not calc_id or not user_login:
+    if not calc_id or not request.user_login:
         raise HTTPException(status_code=400, detail="calc_id и user_login обязательны")
 
     # Запуск асинхронной задачи и ожидание ее завершения
-    pdf_path = await generate_pdf_async(calc_id, user_login)
+    pdf_path = await generate_pdf_async(calc_id, request)
 
     return {"status": "Completed", "pdf_path": pdf_path}
 
