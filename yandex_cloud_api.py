9b8f291379ea773146e199b8f1508928f2fb40e8
badrievad <zzdl4@bk.ru>
2024-10-23 11:01:41 +0300

    Исправил Logger

--- yandex_cloud_api.py
+++ yandex_cloud_api.py
@@ -6,6 +6,7 @@ from botocore.exceptions import (
     ClientError,
     BotoCoreError,
 )
+from logger import logging
 
 
 def yandex_upload_file_s3(file_path, file_name):
@@ -18,19 +19,19 @@ def yandex_upload_file_s3(file_path, file_name):
 
         # Загрузка файла
         s3.upload_file(file_path, BUCKET_NAME, file_name)
-        print(f"File '{file_name}' successfully uploaded to Yandex Cloud.")
+        logging.info(f"File '{file_name}' successfully uploaded to Yandex Cloud.")
 
     except NoCredentialsError:
-        print(
+        logging.error(
             "No credentials provided. Please check your AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY."
         )
     except PartialCredentialsError:
-        print(
+        logging.error(
             "Incomplete credentials provided. Please ensure both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are set."
         )
     except ClientError as e:
-        print(f"Client error occurred: {e}")
+        logging.error(f"Client error occurred: {e}")
     except BotoCoreError as e:
-        print(f"An error occurred in Boto3: {e}")
+        logging.error(f"An error occurred in Boto3: {e}")
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        logging.error(f"An unexpected error occurred: {e}")
